79c92b9c872bf96987b9b4f98eed26fa37459ed2
Damanpreet Singh Grewal <damanpreetsinghgrewal92@gmail.com>
2022-09-27 06:10:29 -0400

    apiSlice with RTK query/mutation for add,update and delete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import TodoList from "./features/todos/TodoList";
+import TodoList from './features/todos/TodoList';
 
 function App() {
-  return <TodoList />
+  return <TodoList />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.createRoot(document.getElementById('root'))
-  .render(
-    <React.StrictMode>
+import { ApiProvider } from '@reduxjs/toolkit/query/react';
+import { apiSlice } from './features/api/apiSlice';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ApiProvider api={apiSlice}>
       <App />
-    </React.StrictMode>
-  );
+    </ApiProvider>
+  </React.StrictMode>
+);
